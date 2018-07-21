a58d8adb475350e0551a1b9ba4b3bebee5217780
Lukasz93P <lukasz9.3@interia.pl>
2018-07-21 08:27:31 +0200

    New slightly better layout

--- src/components/booking/bookings-manage/BookingsCard.js
+++ src/components/booking/bookings-manage/BookingsCard.js
@@ -7,16 +7,16 @@ const BookingCard=(props)=>{
     const {booking}=props
     
     return(
-        <div className='col-6 m-top-4'>
+        <div className='col-6 m-top-4 text-center text-secondary'>
             <div className="card">
             <div className="card-header ">
                 {booking.days} day/s in {booking.rental.title}
             </div>
-            <div className="card-body d-flex-justify-center">
+            <div className="card-body ">
                 <h4 className="card-title">{moment(booking.startAt).format('Y/MM/DD')} - {moment(booking.endAt).format('Y/MM/DD')}</h4>
                 <h5>Price: {booking.totalPrice}</h5>
                 <Link to={`/rentals/${booking.rental._id}`}><button className='btn btn-success'>See booked rental</button></Link>
-                <button className='btn btn-danger'>Cancel booking</button>
+                <button className='btn btn-danger ml-3'>Cancel booking</button>
             </div>
             </div>
         </div>
--- src/components/rental/rental-manage/RentalManageCard.js
+++ src/components/rental/rental-manage/RentalManageCard.js
@@ -7,18 +7,23 @@ const RentalManageCard=(props)=>{
     const {onDelete,rental}=props
     
     return(
-        <div className='col-6 m-top-4'>
+        <div className='col-6 m-top-4 text-center text-secondary'>
             <div className="card">
             <div className="card-header ">
-                {rental.title}
+                <h3>{rental.title}</h3>
             </div>
-            <div className="card-body d-flex-justify-center">
+            <button className="btn btn-primary" type="button" data-toggle="collapse" data-target={`#collapse${rental._id}`} aria-expanded="false" aria-controls={`#collapse${rental._id}`}>
+                Click for details
+            </button>
+            <div class="collapse" id={`collapse${rental._id}`}>
+            <div className="card-body  d-flex-justify-center">
                 <h4 className="card-title">Rental Details</h4>
                 <h5>{rental.city} {rental.street}</h5>
                 <h5>Daily rate: {rental.dailyRate}</h5>
                 <h6>Added at : {moment(rental.createdAt).format('Y/MM/DD')}</h6>
-                <Link to={`/rentals/manage/rentalbookings/${rental._id}`}><button className='btn btn-success'>See bookings</button></Link>
-                <button onClick={()=>onDelete(rental._id)}className='btn btn-danger'>Delete</button>
+                <Link to={`/rentals/manage/rentalbookings/${rental._id}`}><button className='btn btn-success'>See bookings ({rental.bookings.length})</button></Link>
+                <button onClick={()=>onDelete(rental._id)}className='btn btn-danger ml-3'>Delete</button>
+            </div>
             </div>
             </div>
         </div>
